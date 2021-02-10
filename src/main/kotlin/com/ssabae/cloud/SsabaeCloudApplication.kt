0846301f0019394ccce04842edd81c0c00cb8b90
Ssabae <llsb156@gmail.com>
2021-02-10 18:38:37 +0900

    build: init

---
+++ src/main/kotlin/com/ssabae/cloud/SsabaeCloudApplication.kt
@@ -0,0 +1,11 @@
+package com.ssabae.cloud
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SsabaeCloudApplication
+
+fun main(args: Array<String>) {
+	runApplication<SsabaeCloudApplication>(*args)
+}
---
+++ src/test/kotlin/com/ssabae/cloud/SsabaeCloudApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ssabae.cloud
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SsabaeCloudApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
